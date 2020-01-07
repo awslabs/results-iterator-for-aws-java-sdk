55af7971b520528d9d01cce4b68e5d96e5d2709f
Tim Mattison <tim@mattison.org>
2020-01-07 16:39:41 -0500

    Added test for V1 S3 support in the future if necessary

--- java/src/test/java/com/awslabs/aws/iot/resultsiterator/TestResultsIterator.java
+++ java/src/test/java/com/awslabs/aws/iot/resultsiterator/TestResultsIterator.java
@@ -3,6 +3,10 @@ package com.awslabs.aws.iot.resultsiterator;
 import com.amazonaws.services.iot.AWSIotClient;
 import com.amazonaws.services.iot.model.ListThingsRequest;
 import com.amazonaws.services.iot.model.ThingAttribute;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ListBucketsRequest;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
@@ -15,5 +19,18 @@ public class TestResultsIterator {
         ResultsIterator<ThingAttribute> resultsIterator = new ResultsIterator<>(awsIotClient, listThingsRequest);
         List<ThingAttribute> thingAttributes = resultsIterator.iterateOverResults();
         thingAttributes.forEach(System.out::println);
+    }
+
+    @Test
+    @Ignore
+    // NOTE: This test fails because the S3 SDK breaks the mold and doesn't return a "Result" object when listing buckets.
+    //         Instead it returns the list of buckets directly. This may not be implemented because there's really no reason
+    //         to add the complexity into the results iterator for an operation that does not need the iterator.
+    public void shouldListBucketsAndNotThrowAnException() {
+        AmazonS3Client amazonS3Client = (AmazonS3Client) AmazonS3Client.builder().build();
+        ListBucketsRequest listBucketsRequest = new ListBucketsRequest();
+        ResultsIterator<Bucket> resultsIterator = new ResultsIterator<>(amazonS3Client, listBucketsRequest);
+        List<Bucket> buckets = resultsIterator.iterateOverResults();
+        buckets.forEach(System.out::println);
     }
 }
