dce198ba783aee57ce620bd33cf2ae439a899867
Tim Mattison <tim@mattison.org>
2020-01-21 12:52:40 -0500

    Binding IoHelper

--- java/src/main/java/com/awslabs/aws/iot/resultsiterator/helpers/v1/V1HelperModule.java
+++ java/src/main/java/com/awslabs/aws/iot/resultsiterator/helpers/v1/V1HelperModule.java
@@ -1,7 +1,9 @@
 package com.awslabs.aws.iot.resultsiterator.helpers.v1;
 
 import com.awslabs.aws.iot.resultsiterator.helpers.GreengrassIdExtractor;
+import com.awslabs.aws.iot.resultsiterator.helpers.IoHelper;
 import com.awslabs.aws.iot.resultsiterator.helpers.implementations.BasicGreengrassIdExtractor;
+import com.awslabs.aws.iot.resultsiterator.helpers.implementations.BasicIoHelper;
 import com.awslabs.aws.iot.resultsiterator.helpers.v1.implementations.*;
 import com.awslabs.aws.iot.resultsiterator.helpers.v1.interfaces.*;
 import com.google.inject.AbstractModule;
@@ -9,6 +11,7 @@ import com.google.inject.AbstractModule;
 public class V1HelperModule extends AbstractModule {
     @Override
     protected void configure() {
+        bind(IoHelper.class).to(BasicIoHelper.class);
         bind(GreengrassIdExtractor.class).to(BasicGreengrassIdExtractor.class);
         bind(V1GreengrassHelper.class).to(BasicV1GreengrassHelper.class);
         bind(V1CertificateHelper.class).to(BasicV1CertificateHelper.class);
