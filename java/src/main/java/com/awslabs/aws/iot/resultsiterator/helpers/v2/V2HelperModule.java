8e300fa9de57233fba6f05834f3957373f4bc90c
Tim Mattison <tim@mattison.org>
2020-01-21 16:09:49 -0500

    Added first V2 SDK helper

--- java/src/main/java/com/awslabs/aws/iot/resultsiterator/helpers/v2/V2HelperModule.java
+++ java/src/main/java/com/awslabs/aws/iot/resultsiterator/helpers/v2/V2HelperModule.java
@@ -1,4 +1,4 @@
-package com.awslabs.aws.iot.resultsiterator.helpers.v1;
+package com.awslabs.aws.iot.resultsiterator.helpers.v2;
 
 import com.awslabs.aws.iot.resultsiterator.helpers.implementations.BasicGreengrassIdExtractor;
 import com.awslabs.aws.iot.resultsiterator.helpers.implementations.BasicIoHelper;
@@ -6,23 +6,17 @@ import com.awslabs.aws.iot.resultsiterator.helpers.implementations.BasicJsonHelp
 import com.awslabs.aws.iot.resultsiterator.helpers.interfaces.GreengrassIdExtractor;
 import com.awslabs.aws.iot.resultsiterator.helpers.interfaces.IoHelper;
 import com.awslabs.aws.iot.resultsiterator.helpers.interfaces.JsonHelper;
-import com.awslabs.aws.iot.resultsiterator.helpers.v1.implementations.*;
-import com.awslabs.aws.iot.resultsiterator.helpers.v1.interfaces.*;
+import com.awslabs.aws.iot.resultsiterator.helpers.v2.implementations.BasicV2IamHelper;
+import com.awslabs.aws.iot.resultsiterator.helpers.v2.interfaces.V2IamHelper;
 import com.google.inject.AbstractModule;
 
-public class V1HelperModule extends AbstractModule {
+public class V2HelperModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(JsonHelper.class).to(BasicJsonHelper.class);
         bind(IoHelper.class).to(BasicIoHelper.class);
         bind(GreengrassIdExtractor.class).to(BasicGreengrassIdExtractor.class);
 
-        bind(V1GreengrassHelper.class).to(BasicV1GreengrassHelper.class);
-        bind(V1CertificateHelper.class).to(BasicV1CertificateHelper.class);
-        bind(V1ThingHelper.class).to(BasicV1ThingHelper.class);
-        bind(V1PolicyHelper.class).to(BasicV1PolicyHelper.class);
-        bind(V1ThingGroupHelper.class).to(BasicV1ThingGroupHelper.class);
-        bind(V1IamHelper.class).to(BasicV1IamHelper.class);
-        bind(V1RuleHelper.class).to(BasicV1RuleHelper.class);
+        bind(V2IamHelper.class).to(BasicV2IamHelper.class);
     }
 }
