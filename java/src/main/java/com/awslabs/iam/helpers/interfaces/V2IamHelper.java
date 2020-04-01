4f65292655cef7cb1d42ca9a376a78c7bbcc3441
Tim Mattison <tim@mattison.org>
2020-04-01 16:10:44 -0400

    Removed pointless comment

--- java/src/main/java/com/awslabs/iam/helpers/interfaces/V2IamHelper.java
+++ java/src/main/java/com/awslabs/iam/helpers/interfaces/V2IamHelper.java
@@ -8,13 +8,6 @@ import java.util.Optional;
 public interface V2IamHelper {
     Optional<Role> getRole(String name);
 
-    /**
-     * Returns the role object
-     *
-     * @param name
-     * @param optionalAssumeRolePolicyDocument
-     * @return
-     */
     Role createRoleIfNecessary(String name, Optional<String> optionalAssumeRolePolicyDocument);
 
     void attachRolePolicies(Role role, Optional<List<String>> optionalManagedPolicyArns);
