0d8894d75707ac0ddd2917794eed98f94c1aff07
Tim Mattison <tim@mattison.org>
2020-09-11 22:33:27 -0400

    v11.0.0, can handle type erasure when necessary

--- java/src/main/java/com/awslabs/resultsiterator/v2/implementations/BasicV2ReflectionHelper.java
+++ java/src/main/java/com/awslabs/resultsiterator/v2/implementations/BasicV2ReflectionHelper.java
@@ -2,6 +2,7 @@ package com.awslabs.resultsiterator.v2.implementations;
 
 import com.awslabs.iot.helpers.interfaces.GreengrassIdExtractor;
 import com.awslabs.resultsiterator.v2.interfaces.V2ReflectionHelper;
+import com.google.gson.internal.$Gson$Types;
 import io.vavr.control.Try;
 import software.amazon.awssdk.awscore.AwsRequest;
 import software.amazon.awssdk.services.greengrass.GreengrassClient;
@@ -9,16 +10,14 @@ import software.amazon.awssdk.services.greengrass.model.GreengrassRequest;
 import software.amazon.awssdk.services.greengrass.model.GreengrassResponse;
 
 import javax.inject.Inject;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 public class BasicV2ReflectionHelper implements V2ReflectionHelper {
-    private final List<String> methodsToIgnore = new ArrayList<>(Arrays.asList("sdkFields", "commonPrefixes"));
+    private final List<String> methodsToIgnore = new ArrayList<>(Arrays.asList("sdkFields", "commonPrefixes", "copy"));
 
     @Inject
     GreengrassIdExtractor greengrassIdExtractor;
@@ -47,6 +46,10 @@ public class BasicV2ReflectionHelper implements V2ReflectionHelper {
 
     @Override
     public Optional<Method> getMethodWithParameterReturnTypeAndNames(Class clazz, Class parameter, Class returnType, List<String> names) {
+        // To deal with generics we look at signatures if all else fails. We want to look for lists of the expected type and the expected type itself.
+        String expectedListSignature = toGenericListSignature(returnType);
+        String expectedSignature = toGenericSignature(returnType);
+
         Method returnMethod = null;
 
         for (Method method : clazz.getMethods()) {
@@ -79,20 +82,92 @@ public class BasicV2ReflectionHelper implements V2ReflectionHelper {
                 }
             }
 
-            if (!method.getReturnType().isAssignableFrom(returnType)) {
-                // Not the right return type, ignore
+            String currentSignature = toGenericSignature(method.getGenericReturnType());
+
+            if (!method.getReturnType().isAssignableFrom(returnType) &&
+                    !expectedListSignature.equals(currentSignature) &&
+                    !expectedSignature.equals(currentSignature)) {
+                // Not the right return type, signature of a list with the return type, or signature of the return type itself, ignore
                 continue;
             }
 
             if (returnMethod != null) {
                 // More than one match found, fail
-                throw new UnsupportedOperationException("Multiple methods found, cannot continue");
+                throw new UnsupportedOperationException("Multiple methods found, cannot continue. Try using V2ResultsIteratorAbstract as an anonymous class to avoid compile time type erasure.");
             }
 
             returnMethod = method;
         }
 
         return Optional.ofNullable(returnMethod);
+    }
+
+    // From: https://stackoverflow.com/a/29801335/796579
+    private String toGenericListSignature(final Type type) {
+        ParameterizedType listType = $Gson$Types.newParameterizedTypeWithOwner(null, List.class, type);
+        return toGenericSignature(listType);
+    }
+
+    private String toGenericSignature(final Type type) {
+        StringBuilder sb = new StringBuilder();
+        toGenericSignature(sb, type);
+        return sb.toString();
+    }
+
+    private void toGenericSignature(StringBuilder sb, final Type type) {
+        if (type instanceof GenericArrayType) {
+            sb.append("[");
+            toGenericSignature(sb, ((GenericArrayType) type).getGenericComponentType());
+        } else if (type instanceof ParameterizedType) {
+            ParameterizedType pt = (ParameterizedType) type;
+            sb.append('L');
+            sb.append(((Class) pt.getRawType()).getName().replace('.', '/'));
+            sb.append('<');
+            for (Type p : pt.getActualTypeArguments()) {
+                toGenericSignature(sb, p);
+            }
+            sb.append(">;");
+        } else if (type instanceof Class) {
+            Class clazz = (Class) type;
+            if (!clazz.isPrimitive() && !clazz.isArray()) {
+                sb.append('L');
+                sb.append(clazz.getName().replace('.', '/'));
+                sb.append(';');
+            } else {
+                sb.append(clazz.getName().replace('.', '/'));
+            }
+        } else if (type instanceof WildcardType) {
+            WildcardType wc = (WildcardType) type;
+            Type[] lowerBounds = wc.getLowerBounds();
+            Type[] upperBounds = wc.getUpperBounds();
+            boolean hasLower = lowerBounds != null && lowerBounds.length > 0;
+            boolean hasUpper = upperBounds != null && upperBounds.length > 0;
+
+            if (hasUpper && hasLower && Object.class.equals(lowerBounds[0]) && Object.class.equals(upperBounds[0])) {
+                sb.append('*');
+            } else if (hasLower) {
+                sb.append("-");
+                for (Type b : lowerBounds) {
+                    toGenericSignature(sb, b);
+                }
+            } else if (hasUpper) {
+                if (upperBounds.length == 1 && Object.class.equals(upperBounds[0])) {
+                    sb.append("*");
+                } else {
+                    sb.append("+");
+                    for (Type b : upperBounds) {
+                        toGenericSignature(sb, b);
+                    }
+                }
+            } else {
+                sb.append('*');
+            }
+        } else if (type instanceof TypeVariable) {
+            // work around: replaces the type variable with it's first bound.
+            toGenericSignature(sb, ((TypeVariable) type).getBounds()[0]);
+        } else {
+            throw new IllegalArgumentException("Invalid type: " + type);
+        }
     }
 
     @Override
--- java/src/test/java/com/awslabs/resultsiterator/TestV2ResultsIterator.java
+++ java/src/test/java/com/awslabs/resultsiterator/TestV2ResultsIterator.java
@@ -1,14 +1,14 @@
 package com.awslabs.resultsiterator;
 
 import com.awslabs.general.helpers.interfaces.JsonHelper;
-import com.awslabs.resultsiterator.v2.implementations.BouncyCastleV2CertificateCredentialsProvider;
-import com.awslabs.resultsiterator.v2.implementations.DaggerV2TestInjector;
-import com.awslabs.resultsiterator.v2.implementations.V2ResultsIterator;
-import com.awslabs.resultsiterator.v2.implementations.V2TestInjector;
+import com.awslabs.resultsiterator.v2.implementations.*;
 import com.awslabs.resultsiterator.v2.interfaces.V2CertificateCredentialsProvider;
 import com.awslabs.s3.helpers.interfaces.V2S3Helper;
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.RetryPolicy;
 import org.hamcrest.MatcherAssert;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -17,17 +17,16 @@ import software.amazon.awssdk.services.greengrass.GreengrassClient;
 import software.amazon.awssdk.services.greengrass.model.GroupInformation;
 import software.amazon.awssdk.services.greengrass.model.ListGroupsRequest;
 import software.amazon.awssdk.services.iot.IotClient;
-import software.amazon.awssdk.services.iot.model.CreateThingRequest;
-import software.amazon.awssdk.services.iot.model.DeleteThingRequest;
-import software.amazon.awssdk.services.iot.model.ListThingsRequest;
-import software.amazon.awssdk.services.iot.model.ThingAttribute;
+import software.amazon.awssdk.services.iot.model.*;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
 import software.amazon.awssdk.services.s3.model.S3Object;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -38,7 +37,10 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 public class TestV2ResultsIterator {
-    public static final String JUNKFORTESTING_V2 = "JUNKFORTESTINGv2";
+    public static final String JUNKFORTHINGTESTING_V2 = "JUNKFORTHINGTESTINGv2";
+    public static final String JUNKFORGROUPTESTING_V2 = "JUNKFORGROUPTESTINGv2";
+    public static final String THING_GROUP_NAMES = "thingGroupNames";
+    public static final String DELIMITER = ":";
     private final Logger log = LoggerFactory.getLogger(TestV2ResultsIterator.class);
     private IotClient iotClient;
     private GreengrassClient greengrassClient;
@@ -58,17 +60,39 @@ public class TestV2ResultsIterator {
         jsonHelper = injector.jsonHelper();
 
         CreateThingRequest createThingRequest = CreateThingRequest.builder()
-                .thingName(JUNKFORTESTING_V2)
+                .thingName(JUNKFORTHINGTESTING_V2)
                 .build();
         iotClient.createThing(createThingRequest);
+
+        CreateThingGroupRequest createThingGroupRequest = CreateThingGroupRequest.builder()
+                .thingGroupName(JUNKFORGROUPTESTING_V2)
+                .build();
+        iotClient.createThingGroup(createThingGroupRequest);
+
+        DescribeThingRequest describeThingRequest = DescribeThingRequest.builder()
+                .thingName(JUNKFORTHINGTESTING_V2)
+                .build();
+
+        DescribeThingResponse describeThingResponse = iotClient.describeThing(describeThingRequest);
+
+        AddThingToThingGroupRequest addThingToThingGroupRequest = AddThingToThingGroupRequest.builder()
+                .thingArn(describeThingResponse.thingArn())
+                .thingGroupName(JUNKFORGROUPTESTING_V2)
+                .build();
+        iotClient.addThingToThingGroup(addThingToThingGroupRequest);
     }
 
     @After
     public void tearDown() {
         DeleteThingRequest deleteThingRequest = DeleteThingRequest.builder()
-                .thingName(JUNKFORTESTING_V2)
+                .thingName(JUNKFORTHINGTESTING_V2)
                 .build();
         iotClient.deleteThing(deleteThingRequest);
+
+        DeleteThingGroupRequest deleteThingGroupRequest = DeleteThingGroupRequest.builder()
+                .thingGroupName(JUNKFORGROUPTESTING_V2)
+                .build();
+        iotClient.deleteThingGroup(deleteThingGroupRequest);
     }
 
     @Test
@@ -154,5 +178,46 @@ public class TestV2ResultsIterator {
     public void shouldThrowExceptionWhenThingNameNotPresent() {
         RuntimeException exception = assertThrows(RuntimeException.class, () -> v2CertificateCredentialsProvider.resolveCredentials());
         assertTrue(exception.getMessage().contains(BouncyCastleV2CertificateCredentialsProvider.AWS_CREDENTIAL_PROVIDER_URL));
+    }
+
+    private void waitForNonZeroFleetIndexResult(Callable<Stream> streamCallable) {
+        RetryPolicy<Long> fleetIndexRetryPolicy = new RetryPolicy<Long>()
+                .handleResult(0L)
+                .withDelay(Duration.ofSeconds(5))
+                .withMaxRetries(10)
+                .onRetry(failure -> log.warn("Waiting for non-zero fleet index result..."))
+                .onRetriesExceeded(failure -> log.error("Fleet index never returned results, giving up"));
+
+        Failsafe.with(fleetIndexRetryPolicy).get(() -> streamCallable.call().count());
+    }
+
+    @Test
+    public void shouldGetThingDocumentsInsteadOfThingGroups() {
+        SearchIndexRequest searchIndexRequest = getAllThingGroupsSearchIndexRequest();
+
+        V2ResultsIteratorAbstract<ThingDocument> thingDocumentsIterator = new V2ResultsIteratorAbstract<ThingDocument>(iotClient, searchIndexRequest) {
+        };
+
+        // Wait for the fleet index to settle
+        waitForNonZeroFleetIndexResult(thingDocumentsIterator::stream);
+
+        testNotMeaningfulWithout("things in thing groups", thingDocumentsIterator.stream());
+    }
+
+    private SearchIndexRequest getAllThingGroupsSearchIndexRequest() {
+        String queryString = String.join(DELIMITER, THING_GROUP_NAMES, JUNKFORGROUPTESTING_V2);
+
+        return SearchIndexRequest.builder()
+                .queryString(queryString)
+                .build();
+    }
+
+    @Test
+    public void shouldThrowExceptionDueToTypeErasureAmbiguityWhenRequestingSearchIndexResults() {
+        SearchIndexRequest searchIndexRequest = getAllThingGroupsSearchIndexRequest();
+
+        V2ResultsIterator<ThingDocument> thingDocumentsIterator = new V2ResultsIterator<>(iotClient, searchIndexRequest);
+        RuntimeException unsupportedOperationException = Assert.assertThrows(UnsupportedOperationException.class, () -> thingDocumentsIterator.stream().count());
+        Assert.assertTrue(unsupportedOperationException.getMessage().contains("Multiple methods found"));
     }
 }
