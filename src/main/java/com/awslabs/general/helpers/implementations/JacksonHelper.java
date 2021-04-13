28678f0d1750dd4f802f947842c8f215d290c9ea
Tim Mattison <tim@mattison.org>
2021-04-13 17:10:00 -0400

    Added YAML support to JacksonHelper

--- src/main/java/com/awslabs/general/helpers/implementations/JacksonHelper.java
+++ src/main/java/com/awslabs/general/helpers/implementations/JacksonHelper.java
@@ -2,15 +2,21 @@ package com.awslabs.general.helpers.implementations;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import io.vavr.Lazy;
 import io.vavr.control.Try;
 import io.vavr.jackson.datatype.VavrModule;
 
 public class JacksonHelper {
     private static final Lazy<ObjectMapper> lazyObjectMapper = Lazy.of(JacksonHelper::getObjectMapper);
+    private static final Lazy<ObjectMapper> lazyYamlObjectMapper = Lazy.of(JacksonHelper::getYamlObjectMapper);
 
     public static Try<String> toJsonString(Object object) {
         return Try.of(() -> lazyObjectMapper.get().writeValueAsString(object));
+    }
+
+    public static Try<String> toYamlString(Object object) {
+        return Try.of(() -> lazyYamlObjectMapper.get().writeValueAsString(object));
     }
 
     public static Try<byte[]> toJsonBytes(Object object) {
@@ -24,5 +30,9 @@ public class JacksonHelper {
     private static ObjectMapper getObjectMapper() {
         // Get an object mapper that also is able to serialize vavr objects
         return new ObjectMapper().registerModule(new VavrModule());
+    }
+
+    private static ObjectMapper getYamlObjectMapper() {
+        return new ObjectMapper(new YAMLFactory()).registerModule(new VavrModule());
     }
 }
