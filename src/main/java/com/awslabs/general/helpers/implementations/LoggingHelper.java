51a9b486e11c113071e655bd2473ada6b1e7652f
Tim Mattison <tim@mattison.org>
2021-06-22 12:43:23 -0400

    Added logging helper

---
+++ src/main/java/com/awslabs/general/helpers/implementations/LoggingHelper.java
@@ -0,0 +1,28 @@
+package com.awslabs.general.helpers.implementations;
+
+import org.jetbrains.annotations.NotNull;
+
+public class LoggingHelper {
+    public static void info(String message) {
+        System.out.println(getOutputString(message));
+    }
+
+    public static void error(String message) {
+        System.err.println(getOutputString(message));
+    }
+
+    @NotNull
+    private static String getOutputString(String message) {
+        // Go up two stack frames
+        StackTraceElement stackTraceElement = new Exception().getStackTrace()[2];
+
+        // Print the class name, method name, line number, and message
+        String output = String.join(":", stackTraceElement.getClassName(), stackTraceElement.getMethodName(), String.valueOf(stackTraceElement.getLineNumber()));
+
+        if (message != null) {
+            output = String.join(" - ", output, message);
+        }
+
+        return output;
+    }
+}
