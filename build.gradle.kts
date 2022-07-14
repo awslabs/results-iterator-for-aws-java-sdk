edca2a88c4c133cb7f06a01ac50d0c8fe6b477ef
Tim Mattison <tim@mattison.org>
2022-07-14 10:32:44 -0400

    Switching duplicates strategy to INCLUDE to work around a possible GSON immutables issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ gradle.taskGraph.whenReady {
     allTasks
         .filter { it.hasProperty("duplicatesStrategy") } // Because it's some weird decorated wrapper that I can't cast.
         .forEach {
-            it.setProperty("duplicatesStrategy", "EXCLUDE")
+            it.setProperty("duplicatesStrategy", "INCLUDE")
         }
 }
 
